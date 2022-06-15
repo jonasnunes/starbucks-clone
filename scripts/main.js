1ea21ab616e5f51ee1d21326ae1b3269d07b0370
Jonas Nunes <jonas.nunees@yahoo.com.br>
2022-06-14 23:17:31 -0300

    update project
    mudando a cor do menu após a rolagem via javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// menu responsivo
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
@@ -15,3 +16,13 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+// mudar a cor do header após rolagem da página
+function onScroll(){
+
+    if(scrollY > 0){
+        menuResponsivo.classList.add('scroll')      
+    } else{
+        menuResponsivo.classList.remove('scroll')
+    }
+}
